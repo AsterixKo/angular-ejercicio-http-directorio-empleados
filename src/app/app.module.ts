2c9600d702501206f4b100a079de87d7bbd1177c
MSI\moneo <jvr.moneo@gmail.com>
2020-10-26 17:12:16 +0100

    Crear estructura inicial y añadir bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { WorkersComponent } from './shared/components/workers/workers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WorkersComponent
   ],
   imports: [
     BrowserModule,
